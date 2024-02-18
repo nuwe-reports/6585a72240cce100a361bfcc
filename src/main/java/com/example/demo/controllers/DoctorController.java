e16ab086fa2b860b8cab12a5a658c0a219031211
sufigueroa87 <sufigueroa@proton.me>
2024-02-18 15:09:28 +0100

    doctorcontroller test

--- src/main/java/com/example/demo/controllers/DoctorController.java
+++ src/main/java/com/example/demo/controllers/DoctorController.java
@@ -1,25 +1,30 @@
 package com.example.demo.controllers;
 
+import com.example.demo.repositories.*;
 import com.example.demo.entities.Doctor;
-import com.example.demo.repositories.DoctorRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api")
 public class DoctorController {
 
-    private final DoctorRepository doctorRepository;
-
-    public DoctorController(DoctorRepository doctorRepository) {
-        this.doctorRepository = doctorRepository;
-    }
+    @Autowired
+    DoctorRepository doctorRepository;
 
     @GetMapping("/doctors")
     public ResponseEntity<List<Doctor>> getAllDoctors(){
